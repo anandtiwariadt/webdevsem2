1cf7e08bfad064d9c50af31c5273524fe86d2360
anandtiwariadt <anandtiwari060605@gmail.com>
2026-06-18 18:14:57 +0530

    exp-8

---
+++ exp-8/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Props from './components/Props.jsx'
+
+function App() {
+  return (
+    <div>
+      <h1>Student Information</h1>
+      <Props name="John Doe" rollno="12345" course="Computer Science" />
+    </div>
+  )
+}
+
+export default App
